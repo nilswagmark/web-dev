f2e5ceec89d4b9fb42273819bb6ed05145b42e07
nwagmark <nwagmark@mavenoid.com>
2022-03-21 21:32:08 +0100

    la till

--- firstProject/magic.js
+++ firstProject/magic.js
@@ -1,18 +1,35 @@
 let toDoList = ["study", "work"];
 let command = prompt("What do you want to do?")
+function add(x, y) {
+    console.log(x + y);
+}
 while (command !== "quit") {
     if (command === "list") {
-        for (let i = 0; i < toDoList.length; i++) {
-            console.log("${toDoList[i]}")
+        for (element of toDoList) {
+            console.log(element);
         }
     }
-    command = prompt("What do you want to do?")
+    else if (command === "add") {
+        let newElement = prompt("What do you want to add?");
+        toDoList.push(newElement);
+        console.log(`You added ${newElement} to list`);
+    }
+    else if (command === "delete") {
+        index = prompt("Enter the index you want to delete:");
+        toDoList.splice(index, 1);
+        console.log(`You deleted ${toDoList[index]}`);
+    }
+    command = prompt("What do you want to do?");
+}
+
+console.log("You just quit the app");
+
+function subtract(x,y) {
+    console.log(x-y)
 }
 
 
-
-
-// --- begin mavenoid embedded troubleshooter ---
+/* // --- begin mavenoid embedded troubleshooter ---
 (function (m, a, v, e, n, o, i, d) {
     d = m.createElement(e); d.async = true; d.src = "https://app.mavenoid.com" + v;
     i = m.getElementsByTagName(e)[0]; i.parentNode.insertBefore(d, i); n[o] = n[o] || [];
@@ -26,3 +43,18 @@ mavenoid.config = {
 let buttonTextMap = { en: "Get instant help!", de: "Holen Sie sich sofortige Hilfe!", es: "¡Obtenga ayuda instantánea!", it: "Ottieni assistenza immediata!", fr: "Obtenez de l'aide instantanée !"}
 mavenoid.push({ event: "troubleshooter-open", orgName: "KIPPY", position: "right", defaultLang: document.documentElement.lang});
 /* defaultLang: document.documentElement.lang, buttonText: buttonTextMap[lang ?? "en"] */
+
+
+// --- begin mavenoid embedded troubleshooter ---
+/* (function (m, a, v, e, n, o, i, d) {
+    d = m.createElement(e); d.async = true; d.src = "https://app.mavenoid.com" + v;
+    i = m.getElementsByTagName(e)[0]; i.parentNode.insertBefore(d, i); n[o] = n[o] || [];
+})(document, 0, "/embedded/embedded.js", "script", window, "mavenoid");
+// --- end mavenoid embedded troubleshooter ---
+
+mavenoid.config = {
+    noIframe: false,
+    clientId: "001d862834bb11a069cb",
+};
+mavenoid.push({ event: "troubleshooter-open", orgName: "MOBI", position: "right", alwaysOpen: true, folderName: ""});
+setTimeout(() => { mavenoid.push(({event: "troubleshooter-open", orgName: "MOBI", position: "right", alwaysOpen: true, openByDefault: true})) }, 5000); */
--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,25 @@
 console.log("Hello from our first JS file")
+
+let total = 4;
+
+/* if (total === 4) {
+  console.log("You are a superstar!");
+} */
+
+/* const password = 0
+while (password !== 5) {
+  password = prompt("please anter a new password:");
+}
+
+if (password.length >= 6) {
+  console.log("Long enough password")
+} else {
+  console.log("Password is too short")
+}
+
+let lista = ["banan", "äpple"];
+let dict = { FirstName: "Nils", Age: "23" }; */
+
+
+
+
