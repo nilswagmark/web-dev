df98325079a0e788f44c67ebd253f9addfe52bd3
nwagmark <nwagmark@mavenoid.com>
2022-04-12 09:01:31 +0200

    added latest

--- final-project/app3.js
+++ final-project/app3.js
@@ -1,14 +1,31 @@
-colorButton = document.querySelector("button")
+const form = document.querySelector("#myForm");
 
-const changeColor = () => {
+
+/* const knappen = document.querySelector('button');
+
+const textfield = document.querySelector("#textfield");
+
+
+const buttons = document.querySelectorAll("button");
+
+const PrintNumber = () => {
+    console.log(Math.random())
+} */
+ 
+/* knappen.addEventListener("click", PrintNumber); */
+
+/* const changeColor = () => {
     const r = Math.floor(Math.random() * 255 )
     const g = Math.floor(Math.random() * 255 )
     const b = Math.floor(Math.random() * 255 )
     return `rgb(${r}, ${g}, ${b})`;
-}
+} */
 
 
-colorButton.addEventListener (click, function() {
-    const newColor = changeColor()
-    document.body.style.backgroundColor = newColor; 
-})
+/* for (let button of buttons) {
+    button.addEventListener("click", function () {
+        button.style.backgroundColor = changeColor()
+    })
+} */
+
+
