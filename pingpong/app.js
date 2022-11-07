7668df25a2a74a9ab217bdcb6663fddbd0fbef8c
nwagmark <nwagmark@mavenoid.com>
2022-11-07 08:59:46 +0100

    added

---
+++ pingpong/app.js
@@ -0,0 +1,8 @@
+let points1 = []
+let points2 = []
+
+const klick = () => {
+    console.log("klick")
+}
+const button2 = document.querySelector('#button2');
+button2.addEventListener('click', klick);
